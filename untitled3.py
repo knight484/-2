1a0e6ab946c4c28629c72c255b90f8f935c97e03
knight484 <31915696+knight484@users.noreply.github.com>
2020-03-07 00:41:28 +0800

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Mar  6 21:45:57 2020
-
 # -*- encoding=utf-8 -*-
 import xlsxwriter
 import pandas as pd
@@ -11,14 +9,9 @@ from text_anti_brush_function import *
 df=pd.read_excel('Check_Data.xlsx')
 df1=df["辅助核查列"]
 df2=df["汇总ID"]
-
 content_list=df1
-
 source_id_list=df2
 data1=pd.DataFrame({'content':content_list,'source_id':source_id_list})
-
-
-#print(data1)
 test_data=dict(data1['content'])
 print('排列有:')
 k1=[]
@@ -26,7 +19,6 @@ k2=[]
 k3=[]
 k4=[]
 k5=[]
-
 for i,j in permutations(test_data, 2):
     similar=lcs_similarity(str(data1.iloc[i,0]),str(data1.iloc[j,0]))
     #print(i,j,data1.iloc[i,0],data1.iloc[j,0],similar)
@@ -37,7 +29,6 @@ for i,j in permutations(test_data, 2):
     k4.append(df3)
     k5.append(df4)
     k2.append(similar)
-    
 df5=[x + 1 for x in k1]
 df6=[x + 1 for x in k3]
 data3=pd.DataFrame({'k1':df5,'k4':k4,'k3':df6,'k5':k5,'k2':k2})
